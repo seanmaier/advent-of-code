923525ca7107a1f2de7a5cce0a9823b1fad3b5ba
Sean Leon Maier <sean.maier@outlook.de>
2024-12-07 20:46:50 +0100

    Day 4: Added Part 1 Solution

--- day-4/Program.cs
+++ day-4/Program.cs
@@ -5,7 +5,8 @@ class Program
     static void Main(string[] args)
     {
         string[] input = ProcessFile("input.txt");
-        Console.WriteLine(String.Join("\n", input));
+        int amount = Part1(input);
+        Console.WriteLine($"The total amount of found 'XMAS' is {amount}");
     }
 
     public static string[] ProcessFile(string filePath)
@@ -24,5 +25,45 @@ class Program
 
         return null;
     }
+    
+    
+    public static int Part1(string[] input)
+    {
+        var rowLength = input[1].Length;
+        var colLength = input.Length;
+        var totalAmount = 0;
+        
+        (int, int)[] direction = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)];  
+        
+        for (int col = 0; col < colLength; col++)
+        {
+            for (int row = 0; row < rowLength; row++)
+            {
+                foreach (var (dx, dy) in direction)
+                {
+                    if (IsValid(input, col, row, dx, dy)) totalAmount++;
+                }
+            }
+        }
 
+        return totalAmount;
+    }
+
+    public static bool IsValid(string[] grid,int col, int row, int dx, int dy)
+    {
+        var keyword = "XMAS";
+        var keywordLength = keyword.Length;
+
+        for (int i = 0; i < keywordLength; i++)
+        {
+            var r = row + i * dx;
+            var c = col + i * dy;
+            if (r < 0 || r >= grid[0].Length || c < 0 || c >= grid.Length || grid[c][r] != keyword[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
