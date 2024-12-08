d1ffb6f974b67100d21a2b7b3b9b9d76a61fe588
Sean Leon Maier <sean.maier@outlook.de>
2024-12-08 20:46:24 +0100

    Day 6: Added File Processor

--- day-6/Program.cs
+++ day-6/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var input = ProcessFile("input.txt");
+        for (int i = 0; i < input.GetLength(0); i++)
+        {
+            for (int j = 0; j < input.GetLength(1); j++)
+            {
+                Console.Write(input[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static char[,] ProcessFile(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+        var rows = lines.Length;
+        var cols = lines[0].Length;
+        var formatted = new char[rows, cols];
+        
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                formatted[i, j] = lines[i][j];
+            }
+        }
+
+        return formatted;
     }
 }
