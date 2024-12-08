3d72f1d33014f2cddd112f00102182e1d3f3522f
Sean Leon Maier <sean.maier@outlook.de>
2024-12-08 23:36:26 +0100

    Day 6: Added Part 1

--- day-6/Program.cs
+++ day-6/Program.cs
@@ -1,18 +1,14 @@
-﻿namespace day_6;
+﻿using System.Data;
+
+namespace day_6;
 
 class Program
 {
     static void Main(string[] args)
     {
         var input = ProcessFile("input.txt");
-        for (int i = 0; i < input.GetLength(0); i++)
-        {
-            for (int j = 0; j < input.GetLength(1); j++)
-            {
-                Console.Write(input[i, j]);
-            }
-            Console.WriteLine();
-        }
+        var totalMoves = Part1(input);
+        Console.WriteLine($"The total amount of movements were {totalMoves}");
     }
 
     public static char[,] ProcessFile(string filePath)
@@ -32,4 +28,70 @@ class Program
 
         return formatted;
     }
+
+    public static int Part1(char[,] map)
+    {
+        var dy = new[] {-1, 0, 1, 0}; // Up, right, down, left
+        var dx = new[] { 0, 1, 0, -1 };
+        var rows = map.GetLength(0);
+        var cols = map.GetLength(1);
+        var direction = 0; // Upwards
+        
+        var (y, x) = FindGuard(map, rows, cols);
+        map[y, x] = 'X';
+
+        while (y < rows && y >= 0 && x < cols && x >= 0)
+        {
+            var ny = y + dy[direction];
+            var nx = x + dx[direction];
+
+            if (ny < 0 || ny >= rows || nx < 0 || nx >= cols)
+            {
+                break;
+            }
+
+            if (map[ny, nx] != '#')
+            {
+                y = ny;
+                x = nx;
+                map[y, x] = 'X';
+            }
+            else
+            {
+                direction = (direction + 1) % 4;
+            }
+        }
+
+        return CountPositions(map, rows, cols);
+
+    }
+
+    public static (int y, int x) FindGuard(char[,] map, int rows, int cols)
+    {
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                if (map[i, j] == '^') return (i, j);
+            }
+        }
+
+        throw new Exception("Guard could not be found");
+    }
+    
+    public static int CountPositions(char[,] map, int rows, int cols)
+    {
+        var totalAmount = 0;
+        
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                if (map[i, j] == 'X') totalAmount++;
+            }
+        }
+
+        return totalAmount;
+    }
+
 }
