857e794aaebd8235843eb875fdb5d419452c573b
Sean Leon Maier <sean.maier@outlook.de>
2024-12-09 09:31:07 +0100

    Day 7: Added Processor

--- day-7/Program.cs
+++ day-7/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var dict = ProcessFile("input.txt");
+
+        foreach (var line in dict)
+        {
+            Console.WriteLine($"Key: {line.Key}, Values: {string.Join(", ", line.Value)}");
+        }
+    }
+
+    public static Dictionary<int, List<int>> ProcessFile(string filePath)
+    {
+        try
+        {
+            var input = File.ReadAllLines(filePath);
+            var  calibrationValues = new Dictionary<int, List<int>>();
+            
+            foreach (var line in input)
+            {
+                var parts = line.Split(":");
+                var key = int.Parse(parts[0].Trim());
+                var list = parts[1].Trim().Split(" ").Select(int.Parse).ToList();
+                calibrationValues.Add(key, list);
+            }
+
+            return calibrationValues;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("File could not be read.");
+            Console.WriteLine(e.Message);
+            Environment.Exit(1);
+        }
+
+        return null;
     }
 }
