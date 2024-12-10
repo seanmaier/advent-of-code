3c566a8877a109b14f8b65e60b0ada5413556f44
Sean Leon Maier <sean.maier@outlook.de>
2024-12-10 13:56:26 +0100

    Day 8: Added Processor

--- day-8/Program.cs
+++ day-8/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var map = ProcessFile("input.txt");
+
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                Console.Write(map[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static char[,] ProcessFile(string filePath)
+    {
+        var input = File.ReadAllLines(filePath);
+        var rows = input.Length;
+        var cols = input[0].Length;
+        var map = new char[rows, cols];
+        
+        
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                map[i, j] = input[i][j];
+            }
+        }
+
+        return map;
     }
 }
