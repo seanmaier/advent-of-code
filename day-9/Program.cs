15de758e278d6337adf3fed37fc26fe1243690d6
Sean Leon Maier <sean.maier@outlook.de>
2024-12-11 15:29:51 +0100

    Day 9: Added Processing

--- day-9/Program.cs
+++ day-9/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace day_9;
+﻿using System.Runtime.InteropServices;
+
+namespace day_9;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var processed = ProcessFile("input.txt");
+        Console.WriteLine(string.Join(" | ", processed));
+    }
+
+    static char[] ProcessFile(string filePath)
+    {
+        string input = File.ReadAllText(filePath);
+        return input.ToCharArray();
     }
 }
