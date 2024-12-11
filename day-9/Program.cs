2820087207ff6fedc1673cdb9237b4e14c5dee05
Sean Leon Maier <sean.maier@outlook.de>
2024-12-11 22:03:07 +0100

    Day 9: Added Rearranger

--- day-9/Program.cs
+++ day-9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using System.Text;
 
 namespace day_9;
 
@@ -7,16 +8,20 @@ class Program
     static void Main(string[] args)
     {
         var processed = ProcessFile("input.txt");
-        Console.WriteLine(string.Join(" | ", processed));
+        Rearrange(processed);
     }
 
-    static char[] ProcessFile(string filePath)
+    static int[] ProcessFile(string filePath)
     {
         try
         {
-            var input = File.ReadAllText(filePath);
-            return input.ToCharArray();
-
+            var fileContent = File.ReadAllText(filePath);
+            var numbers = fileContent
+                .Where(char.IsDigit)
+                .Select(c => int.Parse(c.ToString()))
+                .ToArray();
+            
+            return numbers;
         }
         catch (Exception e)
         {
@@ -25,6 +30,30 @@ class Program
             Environment.Exit(1);
         }
 
-        return null;
+        return [-1];
+    }
+
+    static char[] Rearrange(int[] disk)
+    {
+        var after = new StringBuilder();
+        var index = 0;
+        var i = 0;
+
+        foreach (var counter in disk)
+        {
+            if (i % 2 == 0)
+            {
+                after.Append(new string((char)(index + '0'), counter)); // Add '0', '1', '2', ... 
+                index++;
+            }
+            else
+            {
+                after.Append(new string('.', counter)); // Add '.' for the entire counter
+            }
+
+            i++;
+        }
+
+        return after.ToString().ToCharArray();
     }
 }
