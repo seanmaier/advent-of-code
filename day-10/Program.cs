a010c8d227dc9f15b59592e12de7f8035fa68df3
Sean Leon Maier <sean.maier@outlook.de>
2024-12-12 13:08:23 +0100

    Day 10: Added Processing

--- day-10/Program.cs
+++ day-10/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var map = ProcessFile("input.txt");
+
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                Console.Write(map[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static char[,] ProcessFile(string filePath)
+    {
+
+        try
+        {
+            var lines = File.ReadLines(filePath).ToArray();
+            var rows = lines.Length;
+            var cols = lines[0].Length;
+            var result = new char[rows, cols];
+
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < cols; j++)
+                {
+                    result[i, j] = lines[i][j];
+                }
+            }
+
+            return result;
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Couldn't process file");
+            Console.WriteLine(e.Message);
+            Environment.Exit(1);
+            return null;
+        }
+        
     }
 }
